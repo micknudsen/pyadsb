6e1428e745000d49d9ec6e3cb49383bf2e7d4bf4
Michael Knudsen <micknudsen@gmail.com>
2020-04-16 21:30:42 +0200

    Renamed class

--- src/pyadsb/algebra.py
+++ src/pyadsb/algebra.py
@@ -2,7 +2,7 @@ from itertools import zip_longest
 from typing import List, Tuple
 
 
-class BinaryPolynomial:
+class Polynomial:
 
     def __init__(self, coefficients: List[int]) -> None:
         reduced_coefficients = [coefficient % 2 for coefficient in coefficients]
@@ -15,28 +15,28 @@ class BinaryPolynomial:
         return len(self._coefficients) - 1
 
     def __eq__(self, other: object) -> bool:
-        if not isinstance(other, BinaryPolynomial):
+        if not isinstance(other, Polynomial):
             return NotImplemented
         return self._coefficients == other._coefficients
 
-    def __add__(self, other: 'BinaryPolynomial') -> 'BinaryPolynomial':
-        return BinaryPolynomial(coefficients=[x + y for x, y in zip_longest(self._coefficients, other._coefficients, fillvalue=0)])
+    def __add__(self, other: 'Polynomial') -> 'Polynomial':
+        return Polynomial(coefficients=[x + y for x, y in zip_longest(self._coefficients, other._coefficients, fillvalue=0)])
 
-    def __sub__(self, other: 'BinaryPolynomial') -> 'BinaryPolynomial':
+    def __sub__(self, other: 'Polynomial') -> 'Polynomial':
         return self + other
 
-    def __mul__(self, other: 'BinaryPolynomial') -> 'BinaryPolynomial':
+    def __mul__(self, other: 'Polynomial') -> 'Polynomial':
         product = [0] * (self.degree + other.degree + 1)
         for i, x in enumerate(self._coefficients):
             for j, y in enumerate(other._coefficients):
                 product[i + j] += x * y
-        return BinaryPolynomial(coefficients=product)
+        return Polynomial(coefficients=product)
 
-    def __divmod__(self, other: 'BinaryPolynomial') -> Tuple['BinaryPolynomial', 'BinaryPolynomial']:
-        zero = BinaryPolynomial(coefficients=[0])
+    def __divmod__(self, other: 'Polynomial') -> Tuple['Polynomial', 'Polynomial']:
+        zero = Polynomial(coefficients=[0])
         q, r = zero, self
         while r != zero and r.degree >= other.degree:
             # The leading coefficient of a non-zero polynomial over Z/2Z is always 1.
-            t = BinaryPolynomial(coefficients=[0] * (r.degree - other.degree) + [1])
+            t = Polynomial(coefficients=[0] * (r.degree - other.degree) + [1])
             q, r = q + t, r - (t * other)
         return q, r
