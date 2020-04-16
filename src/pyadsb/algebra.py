588aca284f600df0137f38b3ee4d79fa72fb9d5b
Michael Knudsen <micknudsen@gmail.com>
2020-04-16 19:25:03 +0200

    Implemented __eq__ method in BinaryPolynomial class

--- src/pyadsb/algebra.py
+++ src/pyadsb/algebra.py
@@ -5,3 +5,6 @@ class BinaryPolynomial:
         while reduced_coefficients and reduced_coefficients[-1] == 0:
             reduced_coefficients.pop(-1)
         self._coefficients = reduced_coefficients
+
+    def __eq__(self, other: 'BinaryPolynomial') -> bool:
+        return self._coefficients == other._coefficients
