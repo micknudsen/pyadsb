7cd804a253f7ce8f8075e243a25cf9b9347fc085
Michael Knudsen <micknudsen@gmail.com>
2020-04-16 19:23:48 +0200

    Implemented initializer in BinaryPolynomial class

--- src/pyadsb/algebra.py
+++ src/pyadsb/algebra.py
@@ -1,2 +1,7 @@
 class BinaryPolynomial:
-    pass
+
+    def __init__(self, coefficients) -> None:
+        reduced_coefficients = [coefficient % 2 for coefficient in coefficients]
+        while reduced_coefficients and reduced_coefficients[-1] == 0:
+            reduced_coefficients.pop(-1)
+        self._coefficients = reduced_coefficients
